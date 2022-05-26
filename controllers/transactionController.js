5ca1cd1112921369867bfc37e4914d19381c0ccb
Tomas <87679143+devtsp@users.noreply.github.com>
2022-05-26 19:48:56 -0300

    add a route to persist new transaction

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -1,11 +1,30 @@
+const { v4: uuid } = require('uuid');
+
 const connectDB = require('../config/dbConn');
+const newDate = require('../utils/formatDate');
 
 const getAllTransactions = async (req, res) => {
 	const connection = await connectDB;
-	const [rows, fields] = await connection.execute(
-		'SELECT * FROM `transactions`'
-	);
+	const [rows, fields] = await connection.execute('SELECT * FROM transactions');
 	res.json(rows);
 };
 
-module.exports = { getAllTransactions };
+const createTransaction = async (req, res) => {
+	const { concept, amount, type } = req.body;
+	if (!concept.trim())
+		return res.status(400).json({ error: '"Concept" field is required' });
+	if (!amount.trim())
+		return res.status(400).json({ error: '"Amount" field is required' });
+	if (!type.trim())
+		return res.status(400).json({ error: '"Type" field is required' });
+	const connection = await connectDB;
+	const id = uuid();
+	const createdAt = newDate();
+	await connection.execute(
+		`INSERT INTO transactions(id,createdAt,concept,amount,type) 
+    VALUES('${id}','${createdAt}','${concept}',${+amount},'${type}')`
+	);
+	res.send({ id, createdAt, concept, amount: +amount, type });
+};
+
+module.exports = { getAllTransactions, createTransaction };
--- routes/transactions.js
+++ routes/transactions.js
@@ -3,6 +3,9 @@ const router = express.Router();
 
 const transactionController = require('../controllers/transactionController');
 
-router.route('/').get(transactionController.getAllTransactions);
+router
+	.route('/')
+	.get(transactionController.getAllTransactions)
+	.post(transactionController.createTransaction);
 
 module.exports = router;
