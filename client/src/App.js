c1242726b199dc3dc38e69f46f1b311449348ccf
Tomas <87679143+devtsp@users.noreply.github.com>
2022-05-28 20:33:54 -0300

    add routes to register login and dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Register from './pages/Register';
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
 
 function App() {
 	return (
 		<div className="App">
-			<header>
-				<h1>STOCKTAKING APP</h1>
-			</header>
-
-			<Register />
+			<Router>
+				<header>
+					<h1>STOCKTAKING APP</h1>
+				</header>
+				<Routes>
+					<Route path="dashboard" element={<Dashboard />} />
+					<Route path="register" element={<Register />} />
+					<Route path="login" element={<Login />} />
+				</Routes>
+			</Router>
 		</div>
 	);
 }
