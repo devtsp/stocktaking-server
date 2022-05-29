596911f70ef3d0092ea1326c79e7a927794cd049
Tomas <87679143+devtsp@users.noreply.github.com>
2022-05-29 04:05:18 -0300

    add hook to fetch data on load from api (auth needed)

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,7 +1,38 @@
 import React from 'react';
+import main from '../api/main';
 
 const Dashboard = () => {
-	return <div>Dashboard</div>;
+	const [balance, setBalance] = React.useState();
+
+	React.useEffect(() => {
+		let isMounted = true;
+		const controller = new AbortController();
+
+		const getBalance = async () => {
+			try {
+				const response = await main.get('/transactions/balance', {
+					signal: controller.signal,
+				});
+				console.log(response.data);
+				isMounted && setBalance(response.data);
+			} catch (error) {
+				console.log(error);
+			}
+		};
+
+		getBalance();
+
+		return () => {
+			isMounted = false;
+			controller.abort();
+		};
+	}, [setBalance]);
+	return (
+		<div>
+			<h1>Dasboard</h1>
+			<ul></ul>
+		</div>
+	);
 };
 
 export default Dashboard;
