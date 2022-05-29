40672f7a3357ba82b3649c7961e6c65577684d73
Tomas <87679143+devtsp@users.noreply.github.com>
2022-05-29 04:03:49 -0300

    main axios api object to default to facilitate import

--- client/src/api/main.js
+++ client/src/api/main.js
@@ -1,3 +1,5 @@
 import axios from 'axios';
 
-export default axios.create({ baseURL: 'http://localhost:5000/' });
+const main = axios.create({ baseURL: 'http://localhost:5000/' });
+
+export default main;
