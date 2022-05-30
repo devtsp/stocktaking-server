39d3ae9a448255662ac5dc6c4100a95ea6b1c358
Tomas <87679143+devtsp@users.noreply.github.com>
2022-05-30 05:39:35 -0300

    typo

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const handleErrors = require('./middleware/handleErrors');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-//MIDDLEWAR
+//MIDDLEWARE
 app.use(credentials);
 app.use(cors(corsOptions));
 app.use(express.urlencoded({ extended: false }));
