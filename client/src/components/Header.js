55dc032621f6522a6fa5cebc15c8631f1203d34f
Tomas <87679143+devtsp@users.noreply.github.com>
2022-05-30 18:01:07 -0300

    add provisory links to navigate preserving state

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import useAuth from '../hooks/useAuth';
+
 const Header = () => {
+	const { auth } = useAuth();
 	return (
 		<header>
 			<Link to="/dashboard">
-				<h1> DASHBOARD</h1>
+				<h1>DASHBOARD</h1>
 			</Link>
+			<Link to="/operate">
+				<h1>OPERATE</h1>
+			</Link>
+			<Link to="/login">
+				<h1>LOGIN</h1>
+			</Link>
+			<span>({auth?.email || 'Logged Out'})</span>
 		</header>
 	);
 };
