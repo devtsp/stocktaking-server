a20b2a6eb5d66f1c02e1d27e1b34061a9bdefa8b
Tomas <87679143+devtsp@users.noreply.github.com>
2022-05-31 02:52:12 -0300

    PersistLogin component

---
+++ client/src/components/PersistLogin.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import useRefreshToken from '../hooks/useAuth';
+import useAuth from '../hooks/useAuth';
+
+const PersistLogin = ({ children }) => {
+	const [isLoading, setIsLoading] = React.useState(true);
+	const refresh = useRefreshToken();
+	const { auth } = useAuth();
+
+	React.useEffect(() => {
+		const verifyRefreshToken = async () => {
+			try {
+				await refresh();
+			} catch (err) {
+				console.error(err.message);
+			} finally {
+				setIsLoading(false);
+			}
+		};
+
+		!auth?.accessToken ? verifyRefreshToken() : setIsLoading(false);
+	}, []);
+
+	return isLoading ? <p>Loading..</p> : children;
+};
+
+export default PersistLogin;
