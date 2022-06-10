4f10317ecb482cbb513010c59ec549db4c16a08d
Tomas <87679143+devtsp@users.noreply.github.com>
2022-06-10 00:45:24 -0300

    add token table to manage refresh tokens separately

--- sql/userQueries.js
+++ sql/userQueries.js
@@ -1,12 +1,13 @@
-const select = email => `SELECT * from users WHERE email = '${email}'`;
+const select = email => {
+	return `SELECT * from users WHERE email = "${email}"`;
+};
 
-const insert = (email, hashedPassword, id) =>
-	`INSERT INTO users(email, password, id) VALUES('${email}', '${hashedPassword}', '${id}')`;
+const insert = (email, hashedPassword, id) => {
+	return `INSERT INTO users(email, password, id) VALUES("${email}", "${hashedPassword}", "${id}")`;
+};
 
-const updateRefreshToken = (id, refreshToken) =>
-	`UPDATE users SET refreshToken = '${refreshToken}' WHERE id = '${id}'`;
+const updateRefreshToken = (id, refreshToken) => {
+	return `UPDATE users SET refreshToken = "${refreshToken}" WHERE id = "${id}"`;
+};
 
-const getByRefreshToken = refreshToken =>
-	`SELECT * from users WHERE refreshToken = '${refreshToken}'`;
-
-module.exports = { select, insert, updateRefreshToken, getByRefreshToken };
+module.exports = { select, insert, updateRefreshToken };
