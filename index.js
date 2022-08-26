5e97fa218aa1d9181c3c277385d805d910ceaf15
Tomas Paez <paeztms@gmail.com>
2022-08-26 00:05:44 -0300

    remove cors defaults

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000;
 //MIDDLEWARE
 app.use(credentials);
 app.use(cors(corsOptions));
-app.use(cors());
+// app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cookieParser());
