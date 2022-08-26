b27cbf79aceb4ec520c56f2ec243ab6440ad4541
Tomas Paez <paeztms@gmail.com>
2022-08-26 00:05:05 -0300

    restore cors and credentials http config

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 //MIDDLEWARE
-// app.use(credentials);
-// app.use(cors(corsOptions));
+app.use(credentials);
+app.use(cors(corsOptions));
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
