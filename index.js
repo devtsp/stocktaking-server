89660aa6f157d7d08ee7930ae6e542cf1a431717
Tomas Paez <paeztms@gmail.com>
2022-08-25 23:49:34 -0300

    remove custom cors opts

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ const PORT = process.env.PORT || 5000;
 
 //MIDDLEWARE
 // app.use(credentials);
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cookieParser());
